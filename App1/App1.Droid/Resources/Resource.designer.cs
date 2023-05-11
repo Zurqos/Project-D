813be7eac9401f4e1b19a0ce1a83fa1819fcb120
Zurqos <1035187@hr.nl>
2023-05-11 09:51:11 +0200

    ?

--- App1/App1.Droid/Resources/Resource.designer.cs
+++ App1/App1.Droid/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace App1.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.93")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
