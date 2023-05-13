3db97aed91a2e2f9308e8ce9a4b727d6ba006300
Zurqos <1035187@hr.nl>
2023-05-13 09:32:15 +0200

    Made a sample code for Person so that we can add someone to the database

---
+++ App1/App1/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace App1
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
