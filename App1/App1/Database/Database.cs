0edd0e18e43f527b798b04b434f82aded0443308
Zurqos <1035187@hr.nl>
2023-05-13 09:30:54 +0200

    Made a sample database just with the Person in it for now with all its connection strings etc

---
+++ App1/App1/Database/Database.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+namespace App1
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Person>().Wait();
+        }
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            return _database.Table<Person>().ToListAsync();
+        }
+        public Task<int> SavePersonAsync(Person person)
+        {
+            return _database.InsertAsync(person);
+        }
+    }
+}
