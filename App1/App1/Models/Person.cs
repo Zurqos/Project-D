bd6657eae6938da9abe22b929fc01529b1d72e58
Zurqos <1035187@hr.nl>
2023-05-16 14:04:25 +0200

    Added passwordfunctioality and password

--- App1/App1/Models/Person.cs
+++ App1/App1/Models/Person.cs
@@ -4,9 +4,26 @@ namespace App1
 {
     public class Person
     {
+        public Person(int id, string name, bool isAdmin, string Password)
+        {
+            Id = id;
+            Name = name;
+            IsAdmin = isAdmin;
+            this.Password = Password;
+        }
+
+        public Person()
+        {
+        }
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsAdmin { get; set; }
+
+        public string Password { get; set; }
+        
     }
+
+    
 }
