56c37fb436bff4a4dc86fb2eb26c689c838ce603
Zurqos <1035187@hr.nl>
2023-05-24 13:56:32 +0200

    Made the redirection work

--- App1/App1/LoginPage.xaml.cs
+++ App1/App1/LoginPage.xaml.cs
@@ -12,23 +12,52 @@ namespace App1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        public bool isPressed;
+        private Task<List<Person>> listOfAllUsers = App.Database.LinqShowAllAsync();
+        public static Person LoggedInUser;
         public LoginPage()
         {
             InitializeComponent();
         }
         
-        private async void NavigateButton_OnCLicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new LoggedInPage());
+        //check if loggedInUser exists in Database
+      
+            
+        
+        private async void NavigateButton_OnCLickedLogin(object sender, EventArgs e)
+        { 
+            isPressed = false;
+            var username = UserNameInput.Text;
+            var password = correctPassword.Text;
+            if (username == null || password == null)
+            {
+                await DisplayAlert("Login Failed", "Username or Password is incorrect", "OK");
+            }
+            else
+            {
+                foreach (var user in listOfAllUsers.Result)
+                {
+                    if (user.Name == username && user.Password == password)
+                    {
+                        isPressed = true;
+                        LoggedInUser = user;
+                        break;
+                    }
+                }
+            }
+            if (isPressed)
+            {
+                await Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                await DisplayAlert("Login Failed", "Username or Password is incorrect", "OK");
+            }
         }
 
         private async void NavigateButton_ForgotPasswordPage(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ForgotPasswordPage());
-        }
-        private async void NavigateButton_OnClicked4(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new MainPage());
         }
     }
 }
