9cefba8097d09447a8f02e5732f09135ba28787d
Zurqos <1035187@hr.nl>
2023-05-24 13:56:45 +0200

    Changed the button

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr.Runtime;
 using Xamarin.Forms;
 
 namespace App1
@@ -15,15 +16,9 @@ namespace App1
         {
             InitializeComponent();
         }
-
-        private async void NavigateButton_OnCLicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new LoginPage());
-        }
-        
         private async void NavigateButton_OnCLicked2(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new HamburgerMenu());
+            await Navigation.PushAsync(new LoginPage());
         }
 
         private async void NavigateButton_OnClicked3(object sender, EventArgs e)
