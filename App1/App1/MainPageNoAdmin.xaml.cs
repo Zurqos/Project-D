9e0fd03a10bb85734a4e22954d050ca388315e56
Zurqos <1035187@hr.nl>
2023-05-25 09:48:11 +0200

    Made new page for accounts with no admin rights

---
+++ App1/App1/MainPageNoAdmin.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr.Runtime;
+using Xamarin.Forms;
+
+namespace App1
+{
+    
+    
+    public partial class MainPageNoAdmin : ContentPage
+    {
+        public MainPageNoAdmin()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnCLicked2NoAdmin(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+        
+        private async void NavigateButton_OnClicked3NoAdmin(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ocrmodulescreen());
+        }
+        
+        private async void NavigateButton_OnClicked4NoAdmin(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new scanresultaatpagina());
+        }
+      
+    }
+}
